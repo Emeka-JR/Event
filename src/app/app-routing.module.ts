c8cdbc6f596eb38faaccb58bf810de2c1438e90a
Emeka-JR <emekaa919@gmail.com>
2024-05-18 19:11:48 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { TicketListComponent } from './ticket-list/ticket-list.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'Home',component:HomeComponent},
-  {path:'Details',component:ProductDetailsComponent}
+  {path:'Details',component:ProductDetailsComponent},
+  {path:'tickets',component:TicketListComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { TicketListComponent } from './ticket-list/ticket-list.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    TicketListComponent
   ],
   imports: [
     BrowserModule,
